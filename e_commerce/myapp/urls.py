8ab08a242bd7885d9bfe2ca56c6b99045a6e9638
Lucas Gomes <lucgms@gmail.com>
2014-06-30 14:36:58 -0700

    Testing paypal ipn

--- e_commerce/myapp/urls.py
+++ e_commerce/myapp/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, url
 from myapp import views 
+from e_commerce.paypal_checkout.paypal_ipn import Endpoint
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'^endpoint/$', EndPoint()),
 )
--- e_commerce/paypal_checkout/paypal_ipn.py
+++ e_commerce/paypal_checkout/paypal_ipn.py
@@ -1,59 +1,3 @@
-"""
-Classes for accepting PayPal's Instant Payment Notification messages in a 
-Django application (or Django-on-App-Engine):
-
-https://www.paypal.com/ipn
-
-Usage:
-
-from paypal import Endpoint  # Or AppEngineEndpoint as Endpoint
-
-class MyEndPoint(Endpoint):
-    def process(self, data):
-        # Do something with valid data from PayPal - e-mail it to yourself,
-        # stick it in a database, generate a license key and e-mail it to the
-        # user... whatever
-        
-    def process_invalid(self, data):
-        # Do something with invalid data (could be from anywhere) - you 
-        # should probably log this somewhere
-
-These methods can optionally return an HttpResponse - if they don't, a 
-default response will be sent.
-
-Then in urls.py:
-
-    (r'^endpoint/$', MyEndPoint()),
-
-"data" looks something like this:
-
-{
-    'business': 'your-business@example.com',
-    'charset': 'windows-1252',
-    'cmd': '_notify-validate',
-    'first_name': 'S',
-    'last_name': 'Willison',
-    'mc_currency': 'GBP',
-    'mc_fee': '0.01',
-    'mc_gross': '0.01',
-    'notify_version': '2.4',
-    'payer_business_name': 'Example Ltd',
-    'payer_email': 'payer@example.com',
-    'payer_id': '5YKXXXXXX6',
-    'payer_status': 'verified',
-    'payment_date': '11:45:00 Aug 13, 2008 PDT',
-    'payment_fee': '',
-    'payment_gross': '',
-    'payment_status': 'Completed',
-    'payment_type': 'instant',
-    'receiver_email': 'your-email@example.com',
-    'receiver_id': 'CXZXXXXXQ',
-    'residence_country': 'GB',
-    'txn_id': '79F58253T2487374D',
-    'txn_type': 'send_money',
-    'verify_sign': 'AOH.JxXLRThnyE4toeuh-.oeurch23.QyBY-O1N'
-}
-"""
 from django.http import HttpResponse
 import urllib
 
@@ -110,3 +54,59 @@ class AppEngineEndpoint(Endpoint):
             payload = urllib.urlencode(args)
         ).content
 
+"""
+Classes for accepting PayPal's Instant Payment Notification messages in a 
+Django application (or Django-on-App-Engine):
+
+https://www.paypal.com/ipn
+
+Usage:
+
+from paypal import Endpoint  # Or AppEngineEndpoint as Endpoint
+
+class MyEndPoint(Endpoint):
+    def process(self, data):
+        # Do something with valid data from PayPal - e-mail it to yourself,
+        # stick it in a database, generate a license key and e-mail it to the
+        # user... whatever
+        
+    def process_invalid(self, data):
+        # Do something with invalid data (could be from anywhere) - you 
+        # should probably log this somewhere
+
+These methods can optionally return an HttpResponse - if they don't, a 
+default response will be sent.
+
+Then in urls.py:
+
+    (r'^endpoint/$', MyEndPoint()),
+
+"data" looks something like this:
+
+{
+    'business': 'your-business@example.com',
+    'charset': 'windows-1252',
+    'cmd': '_notify-validate',
+    'first_name': 'S',
+    'last_name': 'Willison',
+    'mc_currency': 'GBP',
+    'mc_fee': '0.01',
+    'mc_gross': '0.01',
+    'notify_version': '2.4',
+    'payer_business_name': 'Example Ltd',
+    'payer_email': 'payer@example.com',
+    'payer_id': '5YKXXXXXX6',
+    'payer_status': 'verified',
+    'payment_date': '11:45:00 Aug 13, 2008 PDT',
+    'payment_fee': '',
+    'payment_gross': '',
+    'payment_status': 'Completed',
+    'payment_type': 'instant',
+    'receiver_email': 'your-email@example.com',
+    'receiver_id': 'CXZXXXXXQ',
+    'residence_country': 'GB',
+    'txn_id': '79F58253T2487374D',
+    'txn_type': 'send_money',
+    'verify_sign': 'AOH.JxXLRThnyE4toeuh-.oeurch23.QyBY-O1N'
+}
+"""
