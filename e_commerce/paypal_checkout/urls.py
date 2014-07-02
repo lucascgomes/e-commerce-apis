1fafb87d93b747deb16bd78d158015433593c8f5
Lucas Gomes <lucgms@gmail.com>
2014-07-02 18:17:30 +0000

    Paypal working with return page

--- e_commerce/paypal_checkout/urls.py
+++ e_commerce/paypal_checkout/urls.py
@@ -3,4 +3,5 @@ from paypal_checkout import views
 
 urlpatterns = patterns('',
     url(r'^endpoint/$', views.ipn_endpoint, name='ipn_endpoint'),
+    url(r'^return/$', views.auto_return, name='auto_return')
 )
--- e_commerce/paypal_checkout/views.py
+++ e_commerce/paypal_checkout/views.py
@@ -1,4 +1,7 @@
 from paypal_ipn import Endpoint
+from django.template import RequestContext
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
 from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
 
@@ -8,3 +11,8 @@ from django.views.decorators.csrf import csrf_exempt
 def ipn_endpoint(request):
     print "LALALALALALALALALALALALALALALALALALALALALALA"
     Endpoint().__call__(request)
+    return HttpResponse("OKAY")
+
+def auto_return(request):
+    context = RequestContext(request)
+    return render_to_response('paypal_checkout/return_page.html', {}, context)
